5f39c845c139d70251a447a0cf61d44501995b56
rithickjcreamcollar <163294617+rithickjcreamcollar@users.noreply.github.com>
2024-05-04 19:38:37 +0530

    added

---
+++ src/App.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+import logo from "./logo.svg";
+
+import Home from "./pages/home";
+function App() {
+  return <Home />;
+}
+
+export default App;
---
+++ src/pages/home.tsx
@@ -0,0 +1,37 @@
+import "../styles/home.css";
+const Home: React.FC = () => {
+  const scrollToSection2 = () => {
+    const section2 = document.querySelector(".section2");
+    if (section2) {
+      section2.scrollIntoView({
+        behavior: "smooth",
+        block: "start",
+        inline: "nearest",
+      });
+    }
+  };
+  return (
+    <div className="page">
+      <section className="section1" onClick={scrollToSection2}>
+        <p className="jk">jk.</p>
+        <p className="silk">Silk Sarees</p>
+      </section>
+      <section className="section2">
+        <div className="about-us">
+          <p>
+            Welcome to JK Silk Sarees, a leading manufacturer and wholesaler of
+            premium quality sarees. With a rich heritage in the textile
+            industry, we specialize in producing a wide range of sarees,
+            including both polyester and silk variants.
+          </p>
+        </div>
+      </section>
+      <section className="section3">
+        <h2>CONTACT US</h2>
+        <p>This is the third section.</p>
+      </section>
+    </div>
+  );
+};
+
+export default Home;
